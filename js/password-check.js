4d75d327381906b7cf153bb04d92031dc0a8e1b8
Loris-droid <lorishuon8@gmail.com>
2024-11-13 14:59:08 +0100

    commit

--- js/password-check.js
+++ js/password-check.js
@@ -1,9 +1,19 @@
 function checkPasswordStrength(password) {
     let strength = 0;
+    
+    // Vérification de la longueur minimale
     if (password.length >= 8) strength += 1;
+    
+    // Vérification de la présence de majuscules
     if (/[A-Z]/.test(password)) strength += 1;
+    
+    // Vérification de la présence de minuscules
     if (/[a-z]/.test(password)) strength += 1;
+    
+    // Vérification de la présence de chiffres
     if (/\d/.test(password)) strength += 1;
+    
+    // Vérification de la présence de caractères spéciaux
     if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) strength += 1;
     
     return strength;
