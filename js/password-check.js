b1808506d14f99a266f24bab863884c2e6278ce4
Loris-droid <lorishuon8@gmail.com>
2024-11-13 14:44:50 +0100

    nksebcfiepzq

---
+++ js/password-check.js
@@ -0,0 +1,10 @@
+function checkPasswordStrength(password) {
+    let strength = 0;
+    if (password.length >= 8) strength += 1;
+    if (/[A-Z]/.test(password)) strength += 1;
+    if (/[a-z]/.test(password)) strength += 1;
+    if (/\d/.test(password)) strength += 1;
+    if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) strength += 1;
+    
+    return strength;
+}
---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Ajouter un comportement pour le quiz ou d'autres interactions
+});
